0957a6dc3b474a4cd41843a48226655c0f887f5f
Zhengmao <malker@qq.com>
2020-06-01 01:40:05 -0700

    已经可以通过输入的内容来查找玩家了

--- searchplayer/searchhero.js
+++ searchplayer/searchhero.js
@@ -1,20 +1,34 @@
-const url = "http://owapi.io/profile/pc/us/臺灣省長蔡英文-4640";
+// const url = "http://owapi.io/profile/pc/us/臺灣省長蔡英文-4640";
 
+// const url = "http://owapi.io/profile/pc/us/malker-1273";
+const url = "http://owapi.io/profile/pc/us/";
+function getUserID() {
+    var playerid = "";
+    var inputtext = document.getElementById("playerid");
+    playerid = inputtext.value;
+    playerid = playerid.replace(/#/,"-");
+    return playerid;
+}
 function myfunction() {
     var tankrank = "";
+    var damagerank = "";
+    var supportrank = "";
+    var username = "";
+    var userid = getUserID();
+    var newurl = url + userid;
+    // alert(userid);
     // alert("Its a test for the file");
-    fetch(url)
+    fetch(newurl)
         .then((res) => res.json())
         .then((data) => {
-            // alert(data.competitive);
             console.log(data);
             tankrank = data.competitive.tank.rank;
+            damagerank = data.competitive.damage.rank;
+            supportrank =  data.competitive.support.rank;
             if (!tankrank){
                 tankrank = "Unranked";
             }
-
-            alert(tankrank);
-
+            username = data.usename;
         })
 }
 // myfunction();
