c8bce86def53a84834c1ca02e088b07f155bb96a
Zhengmao <malker@qq.com>
2020-05-31 22:36:29 -0700

    查询功能完成了，已经可以从API处获取有效信息了

--- searchplayer/searchhero.js
+++ searchplayer/searchhero.js
@@ -1,8 +1,21 @@
 const url = "http://owapi.io/profile/pc/us/臺灣省長蔡英文-4640";
+
 function myfunction() {
+    var tankrank = "";
+    // alert("Its a test for the file");
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
-            console(data);
+            // alert(data.competitive);
+            console.log(data);
+            tankrank = data.competitive.tank.rank;
+            if (!tankrank){
+                tankrank = "Unranked";
+            }
+
+            alert(tankrank);
+
         })
 }
+// myfunction();
+// alert("Its a test for the file");
