850099c6f485b5617746ab90cf0c1380455b04de
Zhengmao <malker@qq.com>
2020-06-02 22:11:28 -0700

    实现了对用户的问候功能

--- searchplayer/changebackimg.js
+++ searchplayer/changebackimg.js
@@ -5,33 +5,27 @@ function changelanguage() {
     var country = language.split("-");
     language = country[1];
     // alert(language);
-
+    var pagettle = document.getElementById("titleofpage");
     var bgimg = document.getElementById("tiltejpg");
     if (language == "CN"){
         bgimg.style.backgroundImage="url('images/maps/CN.jpg')";
+        pagettle.innerHTML="Hello Chinese user!你好！中文用户！";
     }
     else if (language == "RU"){
         bgimg.style.backgroundImage="url('images/maps/RU.jpg')";
+        pagettle.innerHTML="Hello Russian user! For Mother Russia!";
     }
     else if (language == "GB"){
         bgimg.style.backgroundImage="url('images/maps/GB.jpg')";
+        pagettle.innerHTML="Hello  user! For Mother Russia!";
     }
     else if (language == "FR"){
         bgimg.style.backgroundImage="url('images/maps/FR.jpg')";
+        pagettle.innerHTML="Hello British user! For Mother Russia!";
     }
     else {
         bgimg.style.backgroundImage="url('images/maps/DEF.jpg')";
     }
-    // switch (language) {
-    //     case "CN":
-    //         break;
-    //     case  "US":
-    //         break;
-    //     case "RU":
-    //         break;
-    //     default:
-    //     //默认设置成月球基地
-    // }
 }
 setTimeout(changelanguage,2000);
 // changelanguage();
