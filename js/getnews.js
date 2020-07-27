dfbf3508abe120c15c0f77494bf8abf81fd076bc
Zhengmao <malker@qq.com>
2020-07-27 01:11:45 -0700

    实现了查询新闻的功能

--- js/getnews.js
+++ js/getnews.js
@@ -1,8 +1,15 @@
-var newsurl="http://newsapi.org/v2/everything\?q\=overwatch\&from\=2020-06-03\&sortBy\=publishedAt\&apiKey\=b0eb5e0f24ef42c2ada704c8036d1595";
+var newsurl="http://newsapi.org/v2/everything\?q\=overwatch\&from\=2020-06-30\&sortBy\=publishedAt\&apiKey\=b0eb5e0f24ef42c2ada704c8036d1595";
+// alert("sadasdasda");
 function getnews() {
     fetch(newsurl)
         .then((res) => res.json())
         .then((data) => {
             // 这里是获取到的新闻json内容，data已经是这个内容类型的头了
+            // console.log(data);
+            // alert(data);
+            console.log(data.articles[1].title);
+            document.getElementById("newstitle").innerText=data.articles[1].title;
+
         })
 }
+getnews();
