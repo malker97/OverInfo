f3802090e651ff52d3916ceabfa84dacac7a753e
Zhengmao <malker@qq.com>
2020-07-30 02:19:05 -0700

    在准备完善新闻查询的项目

--- js/getnews.js
+++ js/getnews.js
@@ -1,4 +1,10 @@
 var newsurl="http://newsapi.org/v2/everything\?q\=overwatch\&from\=2020-06-30\&sortBy\=publishedAt\&apiKey\=b0eb5e0f24ef42c2ada704c8036d1595";
+var localdate = new Date();
+var localyear = localdate.getFullYear();
+// alert(localyear);
+// var localdat
+var localdateM_d = localdate.getDate();
+alert(localdateM_d);
 // alert("sadasdasda");
 function getnews() {
     fetch(newsurl)
