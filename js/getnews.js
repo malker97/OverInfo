cd5aaef5ba5fd1f3f78ee39383e4fe5b2704c7f8
Zhengmao <malker@qq.com>
2020-08-03 07:36:47 -0700

    实现了逐行输出新闻的标题，准备更改其他的排列

--- js/getnews.js
+++ js/getnews.js
@@ -1,5 +1,4 @@
-var newsurl="http://newsapi.org/v2/everything\?q\=overwatch\&from\=2020-"
-var date ="07-15\&sortBy\=publishedAt\&apiKey\=b0eb5e0f24ef42c2ada704c8036d1595";
+var newsurl="http://newsapi.org/v2/everything\?q\=overwatch\&from\=2020-07-15\&sortBy\=publishedAt\&apiKey\=b0eb5e0f24ef42c2ada704c8036d1595";
 var localdate = new Date();
 var localyear = localdate.getFullYear();
 // alert(localyear);
@@ -12,14 +11,21 @@ function getnews() {
         .then((res) => res.json())
         .then((data) => {
             // 这里是获取到的新闻json内容，data已经是这个内容类型的头了
-            // console.log(data);
+            console.log(data);
             // alert(data);
 
             console.log(data.articles[1].title);
             document.getElementById("newstitle1").innerText=data.articles[1].title;
             document.getElementById("description1").innerText=data.articles[1].description;
             document.getElementById("newstitle1").href=data.articles[1].url;
-
+            document.getElementById("newsimg1").src=data.articles[1].urlToImage;
+            let output = '';
+            for (var i in data.articles){
+                output = output + "<h1><a class='newstitle" + i +"'>"+data.articles[i].title+"</a></h1>";
+                // output = output + "<h1 class='newstitle" + i +"'>"+"</h1>";
+            }
+            document.getElementById("result").innerHTML = output;
+            alert(output);
         })
 }
 // alert(<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Old soldiers never die, and they don&#39;t fade away.<br><br>We honor the memory of Roberto Draghetti who brought us the voice of Soldier: 76 in Italy. His extraordinary talent and unmistakable voice delivered depth and passion to one of Overwatch’s most iconic heroes. <a href="https://t.co/3D8TzEWdMx">pic.twitter.com/3D8TzEWdMx</a></p>&mdash; Overwatch (@PlayOverwatch) <a href="https://twitter.com/PlayOverwatch/status/1287887276182130688?ref_src=twsrc%5Etfw">July 27, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>);
